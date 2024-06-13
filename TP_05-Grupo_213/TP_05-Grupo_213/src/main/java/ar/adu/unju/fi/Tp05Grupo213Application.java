17601ba09a642de8fda7c287efdb05bc9d26cde1
usuario <usuario@DESKTOP-DM94O17>
2024-06-13 17:56:44 -0300

    Programa Base para el Proyecto Grupal.

---
+++ TP_05-Grupo_213/TP_05-Grupo_213/src/main/java/ar/adu/unju/fi/Tp05Grupo213Application.java
@@ -0,0 +1,13 @@
+package ar.adu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp05Grupo213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp05Grupo213Application.class, args);
+	}
+
+}
---
+++ TP_05-Grupo_213/TP_05-Grupo_213/src/test/java/ar/adu/unju/fi/Tp05Grupo213ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.adu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp05Grupo213ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
